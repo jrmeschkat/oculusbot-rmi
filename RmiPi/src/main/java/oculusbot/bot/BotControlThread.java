11d372c9930d9f27d7ad093591ce9efc62d59f09
rober <rober@DUSK-PC.fritz.box>
2016-10-17 16:46:56 +0200

    * updated pom and config

--- RmiPi/src/main/java/oculusbot/bot/BotControlThread.java
+++ RmiPi/src/main/java/oculusbot/bot/BotControlThread.java
@@ -1,20 +1,6 @@
 package oculusbot.bot;
 
-import static oculusbot.pi.basics.Pins.GPIO_07;
-import static oculusbot.pi.basics.Pins.GPIO_08;
-import static oculusbot.pi.basics.Pins.GPIO_12;
-import static oculusbot.pi.basics.Pins.GPIO_13;
-import static oculusbot.pi.basics.Pins.GPIO_14;
-import static oculusbot.pi.basics.Pins.GPIO_15;
-import static oculusbot.pi.basics.Pins.GPIO_16;
-import static oculusbot.pi.basics.Pins.GPIO_18;
-import static oculusbot.pi.basics.Pins.GPIO_19;
-import static oculusbot.pi.basics.Pins.GPIO_20;
-import static oculusbot.pi.basics.Pins.GPIO_21;
-import static oculusbot.pi.basics.Pins.GPIO_23;
-import static oculusbot.pi.basics.Pins.GPIO_24;
-import static oculusbot.pi.basics.Pins.GPIO_25;
-import static oculusbot.pi.basics.Pins.GPIO_26;
+import static oculusbot.pi.basics.Pins.*;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -69,8 +55,8 @@ public class BotControlThread extends StatusThread {
 
 	@Override
 	protected void setup() {
-		yaw = new MotorThread(GPIO_24, GPIO_25, GPIO_08, GPIO_07, GPIO_19, gpio, 30, false);
-		pitch = new MotorThread(GPIO_14, GPIO_15, GPIO_18, GPIO_23, GPIO_26, gpio, 40, false);
+		yaw = new MotorThread(GPIO_24, GPIO_25, GPIO_08, GPIO_07, GPIO_06, gpio, 30, false);
+		pitch = new MotorThread(GPIO_14, GPIO_15, GPIO_18, GPIO_23, GPIO_05, gpio, 40, false);
 		roll = new MotorThread(GPIO_12, GPIO_16, GPIO_20, GPIO_21, GPIO_13, gpio, 30, true);
 		yaw.start();
 		pitch.start();
